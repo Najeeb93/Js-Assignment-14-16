ccc6c881535654ef96f1f73ccae912b4142594d8
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-16 20:30:41 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -15,6 +15,16 @@
 // document.write(stringArray)
 
 //Question No: 4
+/*Initialize a number array*/
 
-var numberArray = [10,20,40,50];
-document.write(numberArray[0])
+// var numberArray = [10,20,40,50];
+// document.write(numberArray[0])
+
+
+//Question No: 5
+
+/*Initialize a boolean array*/
+
+var booleanArray = [true,false,true,false]
+
+document.write(booleanArray[1])
